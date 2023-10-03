434dbce2e23ab8a41873a871eb5a1ffcd0d72f80
jamiebellthomas <jamiebt@live.co.uk>
2023-10-03 12:02:45 +0100

    finished 2.7 and added notation

--- chapter_2/2.7_vowelness.c
+++ chapter_2/2.7_vowelness.c
@@ -3,10 +3,12 @@
 #include <math.h>
 #include <stdbool.h>
 #include <assert.h>
-
+#include <ctype.h>
+// ... prototype
 bool isvowel(char a);
 
 bool isvowel(char a){
+    // Only 5 cases so a case by case basis switch is ok
     switch(a){
 	case 'a':
 	    return true;
@@ -22,40 +24,24 @@ bool isvowel(char a){
     return false;
 }
 
-char char_switch(char b){
-    switch(b){
-	case 'a':
-	    b = 'A';
-	    return b;
-	case 'e':
-	    b = 'E';
-	    return b;
-	case 'i':
-	    b = 'I';
-	    return b;
-	case 'o':
-	    b = 'O';
-	    return b;
-	case 'u':
-	    b = 'U';
-	    return b;
-    }
-    return b;
-}
-
 void test(void){
     char vowel = 'a';
     char consonant = 'b';
     assert(isvowel(vowel) == true);
     assert(isvowel(consonant) == false);
-    assert(char_switch(vowel) == 'A');
-    assert(char_switch(consonant) == 'b');
 }
 
 int main(void){
+    // run tests and initialise character variable that will be used
     test();
     char a;
+    printf("Enter a string of characters and we'll convert any lower case vowels to upper");
     while((a = getchar())!= '\n' && a != EOF){
-	printf("%c", a);
+	if(isvowel(a) == true){
+	    // This is a function built into <ctype.h>
+	    a = toupper(a);
+	}
+	printf("%c",a);
     }
+    printf("\n");
 }
