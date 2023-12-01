cf72e3ee65dd68fe9683c4b5ec10dbcdcdad6f74
jamiebellthomas <jamiebt@live.co.uk>
2023-12-01 15:22:48 +0000

    Pointer array index function

--- Assesments/BSA/Alloc/alloc.c
+++ Assesments/BSA/Alloc/alloc.c
@@ -15,5 +15,27 @@ bsa* bsa_init(void){
     return &init_bsa;
 }
 
+int pointer_idx(int idx){
+    // This will take a given index and determine what row of the BSA it sits in
+    int val = idx+1, row_idx = 0, range = 1;
+
+    while(range>=val){
+        row_idx++;
+        range*=2;
+    }
+
+    // This loop intentially runs until it's overshot.
+    // Therefore we need to backtrack 1 row
+    row_idx-=1;
+
+    if(row_idx<=BSA_ROWS){
+        fprintf(stderr, "Index exceeds BSA bounds");
+        exit(EXIT_FAILURE);
+    }
+
+
+    return row_idx;
+
+}
 
 void test(void){}
