46006bb8df87805203c5b16b3f8c77f65348a800
jamiebellthomas <jamiebt@live.co.uk>
2023-12-04 01:01:08 +0000

    Improved comments, tested clang compilation

--- Assesments/BSA/Alloc/alloc.c
+++ Assesments/BSA/Alloc/alloc.c
@@ -147,7 +147,7 @@ bool bsa_delete(bsa* b, int indx){
         return false;
     }
     // At this point we know the cell is in use.
-    // Set value to zero and decrement values in row.
+    // Set value to zero and local values count.
     b->row_array[row_idx].row[col_idx] = UNUSED;
     (b->row_array[row_idx].value_count)--;
 
@@ -157,6 +157,7 @@ bool bsa_delete(bsa* b, int indx){
         free(b->row_array[row_idx].row);
         b->row_array[row_idx].row = NULL;
     }
+
     // Decrement values in array, if there is nothing in the array
     // reset max index to -1 (UNUSED) and return
     (b->value_count)--;
@@ -245,65 +246,75 @@ void test(void){
         assert(test_bsa->row_array[i].value_count == 0);
     }
 
-
     assert(pointer_index(0) == 0);
     assert(pointer_index(1) == 1);
     assert(pointer_index(2) == 1);
     assert(pointer_index(3) == 2);
     assert(pointer_index(6) == 2);
     assert(pointer_index(7) == 3);
-    assert(pointer_index(8) == 3);
     assert(pointer_index(25) == 4);
 
+    assert(col_index(1) == 0);
     assert(col_index(9) == 2);
     assert(col_index(14) == 7);
 
 
-   // Set some values
-   assert(bsa_set(test_bsa, 0, 4));
-   assert(bsa_maxindex(test_bsa)==0);
-   assert(test_bsa->row_array[0].row[0] == 4);
-   assert(test_bsa->row_array[0].value_count == 1);
+    // Set some values
+    assert(bsa_set(test_bsa, 0, 4));
+    assert(bsa_maxindex(test_bsa)==0);
+    assert(test_bsa->row_array[0].row[0] == 4);
+    assert(test_bsa->row_array[0].value_count == 1);
    
-   assert(bsa_set(test_bsa, 15, 15));
-   assert(test_bsa->row_array[4].row[0] == 15);
-   assert(test_bsa->row_array[4].value_count == 1);
+    assert(bsa_set(test_bsa, 15, 15));
+    assert(test_bsa->row_array[4].row[0] == 15);
+    assert(test_bsa->row_array[4].value_count == 1);
 
 
-   assert(test_bsa->value_count == 2);
-   assert(bsa_maxindex(test_bsa) == 15);
+    assert(test_bsa->value_count == 2);
+    assert(bsa_maxindex(test_bsa) == 15);
 
-   int test_row = pointer_index(15), test_col = col_index(15);
-   assert(used_cell(test_bsa, test_row, test_col));
-   test_row = pointer_index(14); 
-   test_col = col_index(14);
-   assert(!used_cell(test_bsa, test_row, test_col));
+    int test_row = pointer_index(15), test_col = col_index(15);
+    assert(used_cell(test_bsa, test_row, test_col));
+    test_row = pointer_index(14); 
+    test_col = col_index(14);
+    assert(!used_cell(test_bsa, test_row, test_col));
 
-   assert(*(bsa_get(test_bsa, 0)) == 4);
-   assert(*(bsa_get(test_bsa, 15)) == 15);
-   assert((bsa_get(test_bsa, 1)) == NULL);
-   assert((bsa_get(test_bsa, 14)) == NULL);
+    assert(bsa_get(test_bsa, 0));
+    assert(bsa_get(test_bsa, 15));
+    assert(*(bsa_get(test_bsa, 0)) == 4);
+    assert(*(bsa_get(test_bsa, 15)) == 15);
+    assert((bsa_get(test_bsa, 1)) == NULL);
+    assert((bsa_get(test_bsa, 14)) == NULL);
 
-   bsa_delete(test_bsa, 15);
-   assert(bsa_get(test_bsa, 15) == NULL);
-   assert(test_bsa->max_index == 0);
-   assert(test_bsa->value_count == 1);
-   assert(test_bsa->row_array[4].value_count == 0);
-   assert(test_bsa->row_array[4].row == NULL);
+    // Remove index 15 and make sure everything 
+    // is in check
+    bsa_delete(test_bsa, 15);
+    // NULL pointer returned to this index
+    assert(bsa_get(test_bsa, 15) == NULL);
+    // New max value is index 0
+    assert(test_bsa->max_index == 0);
+    // One value fewer
+    assert(test_bsa->value_count == 1);
+    // Row is now empty ...
+    assert(test_bsa->row_array[4].value_count == 0);
+    // ... Therefore no memory stored here.
+    assert(test_bsa->row_array[4].row == NULL);
 
-   bsa_delete(test_bsa, 0);
-   assert(bsa_get(test_bsa, 15) == NULL);
-   assert(test_bsa->max_index ==UNUSED);
-   assert(test_bsa->value_count == 0);
-   assert(test_bsa->row_array[0].value_count == 0);
-   assert(test_bsa->row_array[0].row == NULL);
+    // Same procedure for the remaining value
+    bsa_delete(test_bsa, 0);
+    // Now we need to make sure max index is reset to unused.
+    assert(bsa_get(test_bsa, 15) == NULL);
+    assert(test_bsa->max_index ==UNUSED);
+    assert(test_bsa->value_count == 0);
+    assert(test_bsa->row_array[0].value_count == 0);
+    assert(test_bsa->row_array[0].row == NULL);
 
     // Test that setting the last active cell to unused sets max_index
     // to -1 (UNUSED)
     bsa_set(test_bsa, 0, 1);
     assert(test_bsa->max_index == 0);
     bsa_set(test_bsa, 0, UNUSED);
-
+ 
 
 
     assert(test_bsa->max_index ==UNUSED);
