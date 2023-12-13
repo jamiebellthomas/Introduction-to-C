99e67dcba39d857e543186d071c5f16fdeb52159
jamiebellthomas <jamiebt@live.co.uk>
2023-12-13 15:32:45 +0000

    reading in words

--- chapter_12/specific.h
+++ chapter_12/specific.h
@@ -3,3 +3,22 @@
 #include <string.h>
 #include <stdbool.h>
 #include <assert.h>
+
+#define MAXTOKENS 100
+#define MAXTOKENSIZE 20
+/*
+Here prog is going to be the series of words we're reading.
+For this primative example, we're going to use a fixed size 2D array
+and ensure these bounds aren't exceeded.
+
+For the extension, we'll make this structure cooler :)
+*/
+struct prog
+{
+    char words[MAXTOKENS][MAXTOKENSIZE];
+    int current_word;
+};
+
+typedef struct prog Program;
+
+
--- chapter_12/zero_one_parse.c
+++ chapter_12/zero_one_parse.c
@@ -14,7 +14,20 @@ END
 The fact that the part of the grammar calls itself (CODE can be followed by CODE)
 shows that parsing is recursive by nature. 
 
-Before starting the CW, let's try and implement this simple parser
+Before starting the CW, let's try and implement this simple parser.
 */
 
 #include "specific.h"
+
+int main(void){
+    // Init space for the input script. 
+    Program* prog = (Program*)calloc(1, sizeof(Program));
+    int i = 0;
+    // Clever little 1 line while loop, reads words in until it cant anymore
+    while(scanf("%s", prog->words[i++]) == 1 && i<MAXTOKENS);
+
+    printf("Parsed OK!");
+
+    free(prog);
+    return 0;
+}
