82a092c66231b685e209316260952db8a362d0f8
jamiebellthomas <jamiebt@live.co.uk>
2023-12-14 12:42:28 +0000

    Started CW parser

---
+++ Assesments/Turtle/Parse/parse.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAXTOKENS 100
+#define MAXTOKENLEN 50
+
+struct prog {
+    int current_word;
+    char** word_array;
+    int array_len;
+};
+
+typedef struct prog Program;
